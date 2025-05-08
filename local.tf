74da0fb6305fdbf1a0cb84b0dbf18799f588d3cb
Sam Hodgkinson <Sam.Hodgkinson@Ensono.com>
2025-05-08 11:56:58 +0100

    Adding logic to locals to not add route tables if enable_route_tables is false

--- local.tf
+++ local.tf
@@ -3,7 +3,7 @@ locals {
     for k, v in var.subnets : k => merge(
       v,
       contains(keys(azurerm_network_security_group.nsg), k) ? { network_security_group = { id = azurerm_network_security_group.nsg[k].id } } : {},
-      contains(keys(module.route_table), k) ? { route_table = { id = module.route_table[k].resource.id } } : {}
+      var.enable_route_tables == true && contains(keys(module.route_table), k) ? { route_table = { id = module.route_table[k].resource.id } } : {}
     )
   }
 }
