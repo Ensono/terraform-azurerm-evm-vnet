36aaad5bcf23c22b080d4fdb2c173771f9925dc2
Sam Hodgkinson <Sam.Hodgkinson@Ensono.com>
2025-05-09 13:25:22 +0000

    Adding Variable for Firewall Ip

--- variables_vnet.tf
+++ variables_vnet.tf
@@ -97,3 +97,9 @@ variable "subscription_id" {
   default     = null
   description = "(Optional) Subscription ID passed in by an external process.  If this is not supplied, then the configuration either needs to include the subscription ID, or needs to be supplied properties to create the subscription."
 }
+
+variable "firewall_ip_address" {
+  type        = string
+  default     = null
+  description = "(Optional) The IP address of the firewall. This is used for routing."
+}
