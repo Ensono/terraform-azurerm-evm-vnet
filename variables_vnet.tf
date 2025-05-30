4448634b79e288e81605b3256d7e4ab196173827
Rhys Bushnell <rhys.bushnell@ensono.com>
2025-05-30 15:47:10 +0100

    feat: add nat_gateway to subnet variable definition

--- variables_vnet.tf
+++ variables_vnet.tf
@@ -85,15 +85,15 @@ variable "subnets" {
         name = string
       })
     })))
+    service_endpoints             = optional(list(string))
+    nsg_rule_names                = optional(list(string), [])
+    route_names                   = optional(list(string), [])
+    bgp_route_propagation_enabled = optional(bool, true) # Option to enable BGP route propagation for its Route Table.
     nat_gateway = optional(object(
       {
         id = string
       }
     ))
-    service_endpoints             = optional(list(string))
-    nsg_rule_names                = optional(list(string), [])
-    route_names                   = optional(list(string), [])
-    bgp_route_propagation_enabled = optional(bool, true) # Option to enable BGP route propagation for its Route Table.
   }))
 }
 
