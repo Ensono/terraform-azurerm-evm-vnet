93d3b99c64501d91ae4fd44e0c6cdc7b41648076
SamHodgkinson <9842003+samhodgkinson@users.noreply.github.com>
2025-06-13 10:16:44 +0100

    Hotfix/2731 vnet outputs (#32)
    * updates to examples
    
    * correcting outputs

--- examples/main/outputs_example.tf
+++ examples/main/outputs_example.tf
@@ -4,14 +4,30 @@ output "resource_group_name" {
   sensitive   = false
 }
 
-output "resource_name" {
+output "vnet_name" {
   description = "The resource name."
   value       = module.example.vnet_name
   sensitive   = false
 }
 
-output "resource_id" {
+output "vnet_resource_id" {
   description = "The resource id."
   value       = module.example.vnet_resource_id
   sensitive   = false
+}
+
+
+output "subnets" {
+  description = "Detailed information about each subnet."
+  value       = module.example.subnets
+}
+
+output "nsg_ids" {
+  description = "The IDs of the network security groups"
+  value       = module.example.nsg_ids
+}
+
+output "route_table_ids" {
+  description = "The IDs of the route tables"
+  value       = module.example.route_table_ids
 }
